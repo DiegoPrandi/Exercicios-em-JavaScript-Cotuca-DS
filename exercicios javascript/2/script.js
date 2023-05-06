542c1b1e6b5f3fd5d3ba1e2cfbc3428072129040
Diego Prandi SIlva <125583321+DiegoPrandi@users.noreply.github.com>
2023-05-05 21:08:58 -0300

    Add files via upload

---
+++ exercicios javascript/2/script.js
@@ -0,0 +1,11 @@
+var num1 = prompt("Digite o primeiro número:");
+var num2 = prompt("Digite o segundo número:");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+
+var soma = num1 + num2;
+
+
+document.write("A soma dos números é: " + soma);
---
+++ exercicios javascript/4/script.js
@@ -0,0 +1,8 @@
+var ra = document.getElementById('ra').value
+var nome = document.getElementById('nome').value
+var idade = document.getElementById('idade').value
+var email = document.getElementById('email').value
+
+if(ra == ''){
+    alert('Ra obrigatório')
+}
