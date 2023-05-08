a4b3f8bcaca5401c344af5e395965c25f4262885
Diego Prandi SIlva <125583321+DiegoPrandi@users.noreply.github.com>
2023-05-08 17:06:52 -0300

    Delete exercicios javascript directory

--- exercicios javascript/4/script.js
+++
@@ -1,8 +0,0 @@
-var ra = document.getElementById('ra').value
-var nome = document.getElementById('nome').value
-var idade = document.getElementById('idade').value
-var email = document.getElementById('email').value
-
-if(ra == ''){
-    alert('Ra obrigatório')
-}
