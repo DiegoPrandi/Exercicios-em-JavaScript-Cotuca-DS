c643465ec5bed13c19c7dd7d184aa8a62f0df7e6
Diego Prandi Silva <125583321+DiegoPrandi@users.noreply.github.com>
2023-05-09 22:07:36 -0300

    Add files via upload

---
+++ exercicios-marcia-main/exercicios javascript/4/script.js
@@ -0,0 +1,23 @@
+function validacao(){
+    var ra = document.getElementById('ra').value
+    var nome = document.getElementById('nome').value
+    var idade = document.getElementById('idade').value
+    var email = document.getElementById('email').value
+
+    if (ra == '' || nome == '' || idade == '' || email == ''){
+        alert('Preencha os campos corretamente.')
+    }
+
+    // --> Conta os números de caracteres dentro da variavel nome
+    if (nome.length > 40) {
+        alert('O campo nome só pode ter no máximo 40 caracteres.')
+    }
+
+    if (idade.length > 3){
+        alert('O campo idade só pode ter no máximo 3 caracteres.')
+    }
+
+    if (!/\S+@\S+\.\S+/.test(email)){
+        alert('Formato de email incorreto, por favor digite certo.')
+    }
+}
